e8e293a617075628d74e1b7c463ecf70487a3b26
Michael Douglas <michaelperezdouglas@gmail.com>
2020-04-14 16:37:53 -0400

    Json Http pull scripts written

--- WeatherAPIProject/Assets/Scripts/APIPull.cs
+++ WeatherAPIProject/Assets/Scripts/APIPull.cs
@@ -1,18 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Networking;
 public class APIPull : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string city;
+    public string state;
+    string url;
+    public CurrentWeather currentWeather;
     void Start()
     {
-        
+        AquireWeather();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AquireWeather()
     {
-        
+        AquireWeather(15218);
+    }
+    public void AquireWeather(string city, string state)
+    {
+        url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "&appid=529b090848369d167a6b1f9acbbf1c0c";
+        StartCoroutine(GetRequest(url));
+    }
+    public void AquireWeather(int zip)
+    {
+        url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip.ToString() + "&appid=529b090848369d167a6b1f9acbbf1c0c";
+        StartCoroutine(GetRequest(url));
+    }
+    public void AquireWeather(string city)
+    {
+        url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=529b090848369d167a6b1f9acbbf1c0c";
+        StartCoroutine(GetRequest(url));
+    }
+    IEnumerator GetRequest(string url)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+        {
+            //Request and wait for the desired url data
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log("Error" + webRequest.error);
+            }
+            else
+            {
+                currentWeather = JsonUtility.FromJson<CurrentWeather>(webRequest.downloadHandler.text);
+            }
+        }
+        //TO DO:Make events happen (Carter)
     }
 }
---
+++ WeatherAPIProject/Assets/Scripts/JSONData.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Custom Class
+[System.Serializable]
+public class CurrentWeather
+{
+    public Place coord = new Place();
+    public Weather[] weather = new Weather[1];
+    public Main main = new Main();
+}
+[System.Serializable]
+public class Place
+{
+    public double lon;
+    public double lat;
+}
+[System.Serializable]
+public class Weather
+{
+    public string main;
+    public string description;
+}
+[System.Serializable]
+public class Main
+{
+    public int temp;
+    public int pressure;
+    public int humidity;
+}
+[System.Serializable]
+public class Wind
+{
+    public int speed;
+    public int deg;
+}
+[System.Serializable]
+public class Cloud
+{
+    public int all;
+}
+
+
+//http://api.openweathermap.org/data/2.5/weather?zip= + zip + &appid=529b090848369d167a6b1f9acbbf1c0c
