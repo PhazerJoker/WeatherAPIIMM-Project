f7b212ed039e68d532336974fae4b02309ed42f8
Carter Bourne <60516083+cbourne2023@users.noreply.github.com>
2020-04-19 21:49:18 -0400

    Revert "Revert "Added weatherdecider script to project""
    This reverts commit e99d1edf6b5b1167fb8efa14a508e03c529dd0ec.

---
+++ WeatherAPIProject/Assets/Scripts/WeatherDecider.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class WeatherDecider : MonoBehaviour
+{
+    public void Setweather(string condition)
+    {
+        TurnOffAll();
+        if(condition == "Sunny")
+        {
+            GameObject[] sunnyObjects = GameObject.FindGameObjectsWithTag("Sunny");
+            foreach(GameObject go in sunnyObjects)
+            {
+                go.SetActive(true);
+            } 
+        }
+    }
+    public void TurnOffAll()
+    {
+        //disables all weather objects
+    GameObject[] sunnyObjects = GameObject.FindGameObjectsWithTag("Sunny");
+            foreach(GameObject go in sunnyObjects)
+            {
+                go.SetActive(false);
+            }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
