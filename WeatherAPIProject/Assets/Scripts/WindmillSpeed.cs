a6f0b5873f62a6b1f8668d35364e613a5c8609bb
Christopher Brinkley <Christopher.Brinkley@chatham.edu>
2020-04-20 18:57:46 -0400

    Added Windmill Model and WindmillSpeed script

---
+++ WeatherAPIProject/Assets/Scripts/WindmillSpeed.cs
@@ -0,0 +1,34 @@
+﻿//Script for setting the speed for the Windmill
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindmillSpeed : MonoBehaviour
+{
+    public Wind wind;
+    public GameObject windMill;
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (wind.speed > 1 && wind.speed <= 5)
+        {
+            windMill.transform.Rotate(10 * Time.deltaTime, 0, 0);
+        }else if (wind.speed > 5 && wind.speed <= 20)
+        {
+            windMill.transform.Rotate (50*Time.deltaTime,0,0);
+        }else if (wind.speed > 20 && wind.speed <= 50)
+        {
+            windMill.transform.Rotate (100*Time.deltaTime,0,0);
+        }
+        else if (wind.speed > 50)
+        {
+            windMill.transform.Rotate (200*Time.deltaTime,0,0);
+        }
+        else
+        {
+            windMill.transform.Rotate (0,0,0);
+        }
+    }
+}
