4392cd78827921b42bb6edfdeadff1911d0134bf
Michael Douglas <michaelperezdouglas@gmail.com>
2020-04-21 15:14:12 -0400

    Script fixed they all work, weather is doing things well

---
+++ WeatherAPIProject/Assets/Scripts/Activator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    public GameObject[] activateOnStart;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach (GameObject go in activateOnStart)
+        {
+            go.SetActive(true);
+        }
+    }
+}
